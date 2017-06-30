bc8ec9a9bc5fec54a15362477a0578a7b2805bfc
Marc Deheeger <mcdeheeger@Marcs-MacBook-Pro.local>
2017-06-29 21:40:17 -0500

    initial setup

--- app/app.js
+++
@@ -1,9 +0,0 @@
-// Include the Main React Dependencies
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Parent from "./components/Parent";
-
-
-// This code here allows us to render our main component (in this case "Parent")
-ReactDOM.render(<Parent />, document.getElementById("app"));
